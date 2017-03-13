c955e862d5a878f8ff549966e28af7b2b6983c22
Oliver Bell <freshollie@gmail.com>
2017-03-13 00:27:37 +0000

    Created initial activity

---
+++ project/app/DisasterZone/app/src/main/java/uk/ac/coventry/m206cde/tutorial3/group5/disasterzone/SplashScreenActivity.java
@@ -0,0 +1,13 @@
+package uk.ac.coventry.m206cde.tutorial3.group5.disasterzone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
