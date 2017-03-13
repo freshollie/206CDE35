0306715d5f419978f54655d1d0950fc65f7beea0
Oliver Bell <freshollie@gmail.com>
2017-03-13 00:59:36 +0000

    Added layout for splashscreen

--- project/app/DisasterZone/app/src/main/java/uk/ac/coventry/m206cde/tutorial3/group5/disasterzone/SplashScreenActivity.java
+++ project/app/DisasterZone/app/src/main/java/uk/ac/coventry/m206cde/tutorial3/group5/disasterzone/SplashScreenActivity.java
@@ -1,7 +1,9 @@
 package uk.ac.coventry.m206cde.tutorial3.group5.disasterzone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        findViewById(R.id.get_started_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), DisasterListActivity.class));
+            }
+        });
+
     }
 }
